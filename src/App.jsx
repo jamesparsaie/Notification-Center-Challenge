f57f59007425fe67face44d65b14bb062c0ff9f6
jamesparsaie <parsaiejames@gmail.com>
2023-12-06 17:56:44 -0600

    4 messages done, need to add marking as read

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,10 @@ function App() {
         <br>
         </br>
 
-        <div className='row'>
+        <div className='row row-inactive'>
           <Avatar src='assets/images/avatar-mark-webber.webp'/>
           <div className='row-content'>
-            <p><b>Mark Webber</b> reacted to your recent post <span id='post-highlight'>My first tournament today!</span></p>
+            <p><span className='name'>Mark Webber</span> reacted to your recent post <span id='post-highlight'>My first tournament today!</span></p>
             <p id='time'>1m ago</p>
           </div>
           <GoDotFill  id='red-dot'/>
@@ -32,10 +32,10 @@ function App() {
 
         <br></br>
 
-        <div className='row'>
+        <div className='row row-inactive'>
           <Avatar src='assets/images/avatar-angela-gray.webp' />
           <div className='row-content'>
-            <p><b>Angela Gray</b> followed you</p>
+            <p><span className='name'>Angela Gray</span> followed you</p>
             <p id='time'>5m ago</p>
           </div>
           <GoDotFill  id='red-dot'/>
@@ -43,9 +43,31 @@ function App() {
 
         <br></br>
 
-        <div className='row'>
-
+        <div className='row row-inactive'>
+          <Avatar src='assets\images\avatar-jacob-thompson.webp' />
+          <div className='row-content'>
+            <p><span className='name'>Jacob Thompson</span> has joined your group Chess Club </p>
+            <p id='time'>1 day ago</p>
+          </div>
+          <GoDotFill  id='red-dot'/>
         </div> 
+
+        <br></br>
+
+        <div className='row row-active'>
+          <Avatar src='assets\images\avatar-rizky-hasanuddin.webp' />
+          <div className='row-content'>
+            <p><span className='name'>Rizky Hasanuddin</span> sent you a private message</p>
+            <p id='time'>5 days ago</p>
+            <div className='private-message'>
+              <p>Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having
+                lots of fun and improving my game</p>
+            </div>
+          </div>
+        </div>
+
+        <br></br>
+
       </div>
     </div>
   )
